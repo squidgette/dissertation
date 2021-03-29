f1712eb541735a5aaa27c28f6241d342c34fbdc2
squidgette <81539438+squidgette@users.noreply.github.com>
2021-03-28 17:59:53 -0700

    Create mobilesurvey.js

---
+++ mobilesurvey.js
@@ -0,0 +1,122 @@
+define(['pipAPI','https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualtrics/quiat9.js'], function(APIConstructor, iatExtension){
+	var API = new APIConstructor();
+  
+  //Randomly choose the attribute words from a larger list of attribute words.
+  //Internal studies in Project Implicit found no difference in performance between the words.
+  var posWords = API.shuffle([
+            'Love', 'Cheer', 'Friend', 'Pleasure', 
+            'Adore', 'Cheerful', 'Friendship', 'Joyful', 
+            'Smiling','Cherish', 'Excellent', 'Glad', 
+            'Joyous', 'Spectacular', 'Appealing', 'Delight', 
+            'Excitement', 'Laughing', 'Attractive','Delightful', 
+            'Fabulous', 'Glorious', 'Pleasing', 'Beautiful', 
+            'Fantastic', 'Happy', 'Lovely', 'Terrific', 
+            'Celebrate', 'Enjoy', 'Magnificent', 'Triumph']);
+  var negWords = API.shuffle([
+            'Abuse', 'Grief', 'Poison', 'Sadness', 
+            'Pain', 'Despise', 'Failure', 'Nasty', 
+            'Angry', 'Detest', 'Horrible', 'Negative', 
+            'Ugly', 'Dirty', 'Gross', 'Evil', 
+            'Rotten','Annoy', 'Disaster', 'Horrific',  
+            'Scorn', 'Awful', 'Disgust', 'Hate', 
+            'Humiliate', 'Selfish', 'Tragic', 'Bothersome', 
+            'Hatred', 'Hurtful', 'Sickening', 'Yucky']);
+  
+    return iatExtension({
+      var iatObj =
+		{
+			fullscreen:true, //Should we show the task in full screen? A Qualtrics-only feature because in the usual Minno, we can go full-screen right at the beginning of the study.
+        
+			isTouch:true, //Set whether the task is on a touch device.
+			//Set the canvas of the task
+			canvas : {
+				maxWidth: 725,
+				proportions : 0.7,
+				background: '#ffffff',
+				borderWidth: 5,
+				canvasBackground: '#ffffff',
+				borderColor: 'lightblue'
+			},
+        category1 : {
+            name : 'Asian people', //Will appear in the data.
+            title : {
+                media : {word : 'Asian people'}, //Name of the category presented in the task.
+                css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
+                height : 4 //Used to position the "Or" in the combined block.
+            }, 
+ stimulusMedia : [ //Stimuli content as PIP's media objects
+            {image: 'AFE851.jpg'},
+            {image: 'AFE831.jpg'},
+            {image: 'AFE811.jpg'},
+            {image: 'AME871.jpg'},
+            {image: 'AME831.jpg'},
+            {image: 'AME821.jpg'}
+			], 
+			//Stimulus css (style)
+			stimulusCss : {color:'#31940F','font-size':'2.3em'}
+		},	
+        category2 :    {
+            name : 'White people', //Will appear in the data.
+            title : {
+                media : {word : 'White people'}, //Name of the category presented in the task.
+                css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
+                height : 4 //Used to position the "Or" in the combined block.
+            }, 
+            stimulusMedia : [ //Stimuli content as PIP's media objects
+                {image: 'wm1_nc.jpg'},
+                {image: 'wm2_nc.jpg'},
+                {image: 'wm3_nc.jpg'},
+				{image: 'wf1_nc.jpg'},
+            	{image: 'wf2_nc.jpg'},
+            	{image: 'wf3_nc.jpg'}
+            ],
+    		//Stimulus css (style)
+    		stimulusCss : {color:'#31940F','font-size':'2.3em'}
+        },
+		attribute1 :
+		{
+			name : 'Bad',
+			title : {
+				media : {word : 'Bad'},
+				css : {color:'#0000FF','font-size':'1.8em'},
+				height : 4 //Used to position the "Or" in the combined block.
+			},
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+				{word: negWords[0]},
+				{word: negWords[1]},
+				{word: negWords[2]},
+				{word: negWords[3]},
+				{word: negWords[4]},
+				{word: negWords[5]},
+				{word: negWords[6]},
+				{word: negWords[7]}
+			],
+			//Stimulus css
+			stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+		},
+		attribute2 :
+		{
+			name : 'Good',
+			title : {
+				media : {word : 'Good'},
+				css : {color:'#0000FF','font-size':'1.8em'},
+				height : 4 //Used to position the "Or" in the combined block.
+			},
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+				{word: posWords[0]},
+				{word: posWords[1]},
+				{word: posWords[2]},
+				{word: posWords[3]},
+				{word: posWords[4]},
+				{word: posWords[5]},
+				{word: posWords[6]},
+				{word: posWords[7]}
+			],
+			//Stimulus css
+			stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+		},
+        base_url : {//Where are your images at? You might want to copy the photos to your own server
+            image : 'https://baranan.github.io/minno-tasks/images/'
+        }
+    });
+});
