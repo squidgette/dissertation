4e16cc8ce892dec62c170735cbf0476a7c02e416
squidgette <81539438+squidgette@users.noreply.github.com>
2021-03-28 18:48:02 -0700

    Update desktopsurvey.js
    added CSV logger

--- desktopsurvey.js
+++ desktopsurvey.js
@@ -1,6 +1,40 @@
 define(['pipAPI','https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualtrics/quiat9.js'], function(APIConstructor, iatExtension){
 	var API = new APIConstructor();
-  
+  		API.addSettings('onEnd', window.minnoJS.onEnd);
+		
+		API.addSettings('logger', {
+    // gather logs in array
+    onRow: function(logName, log, settings, ctx){
+        if (!ctx.logs) ctx.logs = [];
+        ctx.logs.push(log);
+    },
+    // onEnd trigger save (by returning a value)
+    onEnd: function(name, settings, ctx){
+        return ctx.logs;
+    },
+    // Transform logs into a string
+    // we save as CSV because qualtrics limits to 20K characters and this is more efficient.
+    serialize: function (name, logs) {
+        var headers = ['group', 'latency', 'block', 'stimulus', 'correct'];
+        var content = logs.map(function (log) { return [log.data.alias, log.latency, log.data.block, log.data.stimIndex, log.data.score]; });
+        content.unshift(headers);
+        return toCsv(content);
+
+        function toCsv(matrice) { return matrice.map(buildRow).join('\n'); }
+        function buildRow(arr) { return arr.map(normalize).join(','); }
+        // wrap in double quotes and escape inner double quotes
+        function normalize(val) {
+            var quotableRgx = /(\n|,|")/;
+            if (quotableRgx.test(val)) return '"' + val.replace(/"/g, '""') + '"';
+            return val;
+        }
+    },
+    // Set logs into an input (i.e. put them wherever you want)
+    send: function(name, serialized){
+        window.minnoJS.logger(serialized);
+    }
+});	
+	
   //Randomly choose the attribute words from a larger list of attribute words.
   //Internal studies in Project Implicit found no difference in performance between the words.
   var posWords = API.shuffle([
